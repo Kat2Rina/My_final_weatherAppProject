4c9efb3afec3ca0e54cf466def0f98180e48c88f
Karina <777katrina@gmail.com>
2024-04-30 17:58:08 +0200

    Added JS template

--- src/index.js
+++ src/index.js
@@ -54,7 +54,32 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="weather-forecast-day"> 
+              <div class="weather-forecast-date">${day}</div>
+
+              <div class="weather-forecast-icon">🌤️</div>
+              <div class="weather-forecast-temp">
+                <span class="weather-forecast-temp-max">18˚</span>
+                <span class="weather-forecast-temp-min">12˚</span>
+              </div>
+              </div>
+           `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Valencia");
+displayForecast();
